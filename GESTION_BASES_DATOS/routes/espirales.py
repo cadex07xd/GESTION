72a5165f380ae72f7b3d4e8d524b3c791e994861
cadex07xd <cesarda.camachomo@ecci.edu.co>
2026-05-19 19:24:47 -0500

    Update espirales.py

--- GESTION_BASES_DATOS/routes/espirales.py
+++ GESTION_BASES_DATOS/routes/espirales.py
@@ -1,13 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_required
-
+from flask_login import login_required, current_user
 from extensions import db
 from models.espiral import Espiral
 from models.maquina import Maquina
 from models.snack import Snack
-if current_user.rol not in ['ADMIN', 'OPERARIO']:
-    flash('No tienes permiso para realizar esta acción', 'danger')
-    return redirect(url_for('home'))
 
 espirales = Blueprint('espirales', __name__)
 
@@ -15,60 +11,41 @@ espirales = Blueprint('espirales', __name__)
 @espirales.route('/espirales')
 @login_required
 def listar_espirales():
-
     lista_espirales = Espiral.query.all()
-
-    return render_template(
-        'espirales/listar.html',
-        espirales=lista_espirales
-    )
+    return render_template('espirales/listar.html', espirales=lista_espirales)
 
 # CREAR
 @espirales.route('/espirales/crear', methods=['GET', 'POST'])
 @login_required
 def crear_espiral():
-
+    if current_user.rol not in ['ADMIN', 'OPERARIO']:
+        flash('No tienes permiso para realizar esta acción', 'danger')
+        return redirect(url_for('home'))
     maquinas = Maquina.query.all()
     snacks = Snack.query.all()
-
     if request.method == 'POST':
-
         nuevo_espiral = Espiral(
-
             codigo=request.form['codigo'],
-
             capacidad=request.form['capacidad'],
-
             stock_actual=request.form['stock_actual'],
-
             maquina_id=request.form['maquina_id'],
-
             snack_id=request.form['snack_id']
         )
-
         db.session.add(nuevo_espiral)
         db.session.commit()
-
         flash('Espiral creado correctamente', 'success')
-
         return redirect(url_for('espirales.listar_espirales'))
-
-    return render_template(
-        'espirales/crear.html',
-        maquinas=maquinas,
-        snacks=snacks
-    )
+    return render_template('espirales/crear.html', maquinas=maquinas, snacks=snacks)
 
 # ELIMINAR
 @espirales.route('/espirales/eliminar/<int:id>')
 @login_required
 def eliminar_espiral(id):
-
+    if current_user.rol not in ['ADMIN', 'OPERARIO']:
+        flash('No tienes permiso para realizar esta acción', 'danger')
+        return redirect(url_for('home'))
     espiral = Espiral.query.get_or_404(id)
-
     db.session.delete(espiral)
     db.session.commit()
-
     flash('Espiral eliminado', 'danger')
-
     return redirect(url_for('espirales.listar_espirales'))
