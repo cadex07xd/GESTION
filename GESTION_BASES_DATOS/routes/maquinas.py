ca6d6799e40fb9c3bb0fd99055b286842c1e9e82
cadex07xd <cesarda.camachomo@ecci.edu.co>
2026-05-19 19:28:35 -0500

    Update maquinas.py

--- GESTION_BASES_DATOS/routes/maquinas.py
+++ GESTION_BASES_DATOS/routes/maquinas.py
@@ -5,19 +5,17 @@ from models.maquina import Maquina
 
 maquinas = Blueprint('maquinas', __name__)
 
-# LISTAR MAQUINAS
 @maquinas.route('/maquinas')
 @login_required
 def listar_maquinas():
     lista_maquinas = Maquina.query.all()
     return render_template('maquinas/listar.html', maquinas=lista_maquinas)
 
-# CREAR MAQUINA
 @maquinas.route('/maquinas/crear', methods=['GET', 'POST'])
 @login_required
 def crear_maquina():
     if current_user.rol not in ['ADMIN', 'OPERARIO']:
-        flash('No tienes permiso para realizar esta acción', 'danger')
+        flash('No tienes permiso', 'danger')
         return redirect(url_for('home'))
     if request.method == 'POST':
         nueva_maquina = Maquina(
@@ -31,12 +29,11 @@ def crear_maquina():
         return redirect(url_for('maquinas.listar_maquinas'))
     return render_template('maquinas/crear.html')
 
-# EDITAR MAQUINA
 @maquinas.route('/maquinas/editar/<int:id>', methods=['GET', 'POST'])
 @login_required
 def editar_maquina(id):
     if current_user.rol not in ['ADMIN', 'OPERARIO']:
-        flash('No tienes permiso para realizar esta acción', 'danger')
+        flash('No tienes permiso', 'danger')
         return redirect(url_for('home'))
     maquina = Maquina.query.get_or_404(id)
     if request.method == 'POST':
@@ -48,12 +45,11 @@ def editar_maquina(id):
         return redirect(url_for('maquinas.listar_maquinas'))
     return render_template('maquinas/editar.html', maquina=maquina)
 
-# ELIMINAR MAQUINA
 @maquinas.route('/maquinas/eliminar/<int:id>')
 @login_required
 def eliminar_maquina(id):
     if current_user.rol not in ['ADMIN', 'OPERARIO']:
-        flash('No tienes permiso para realizar esta acción', 'danger')
+        flash('No tienes permiso', 'danger')
         return redirect(url_for('home'))
     maquina = Maquina.query.get_or_404(id)
     db.session.delete(maquina)
